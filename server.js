b25eae1bdfef8cf5e9189917ee1570a7f4770caf
salmanjaveed <salmanjaveed@gmail.com>
2017-05-05 05:59:30 +0530

    Chaned server.js to send JSON error messages

--- server.js
+++ server.js
@@ -199,14 +199,14 @@ app.post('/create-user-app', function (req, res) {
            var dbString = hash(password, salt);
            pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
               if (err) {
-                  res.status(500).send(err.toString());
+                  res.status(500).send(JSON.stringify({'error' : err.toString()}));
               } else {
                   res.send(JSON.stringify({'message' : 'User successfully created: ' + username}));
               }
            });
      }
     else {
-           res.status(403).send('username/password is invalid');
+           res.status(403).send(JSON.stringify({'error' : 'username/password is invalid'}));
       }
 });
 
@@ -258,17 +258,17 @@ app.post('/login-app', function (req, res) {
        pool.query('SELECT * FROM "user" WHERE username = $1', [username], function (err, result) {
           if (err) {
              // alert(res.send(err.toString()));
-              res.status(500).send(err.toString());
+              res.status(500).send(JSON.stringify({'error': err.toString()}));
           } else {
               if (result.rows.length === 0) {
-                  res.status(403).send('username/password is invalid');
+                  res.status(403).send(JSON.stringify({'error':'username/password is invalid'}));
               } else {
                   // Match the password
                   var dbString = result.rows[0].password;
                   var salt = dbString.split('$')[2];
                   var hashedPassword = hash(password, salt); // Creating a hash based on the password submitted and the original salt
                   if (hashedPassword === dbString) {
-                    
+                     
                     // Set the session
                     req.session.auth = {userId: result.rows[0].id};
                     // set cookie with a session id
@@ -277,7 +277,7 @@ app.post('/login-app', function (req, res) {
                     res.send(JSON.stringify({'Credentials': 'Correct!'}));
                     
                   } else {
-                    res.status(403).send('username/password is invalid');
+                    res.status(403).send(JSON.stringify({'error': 'username/password is invalid'}));
                   }
               }
           }
